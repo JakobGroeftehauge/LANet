015eea879a4407f8540bd64c9c9cdc6ee2316056
JakobGroeftehauge <jakob.groeftehauge@gmail.com>
2022-11-25 10:48:30 +0100

    Added saving a frozen model as .pb file. Not tested yet

--- LANet/src/model.py
+++ LANet/src/model.py
@@ -103,6 +103,7 @@ class relativeHDR(object):
         
         self.save(self.args.checkpoint_dir, self.counter)
         print("Saved checkpoint with counter: ", self.counter)
+        self.freeze_ckpt_graph_and_save(self.args.checkpoint_dir, "generator/out/Conv/Conv2D")
         return 
 
 
@@ -366,3 +367,76 @@ class relativeHDR(object):
                 self.sess.run(q_train.close())
             except Exception as e:
                 pass
+
+
+def freeze_ckpt_graph_and_save(model_dir, output_node_names):
+    """Extract the sub graph defined by the output nodes and convert 
+    all its variables into constant 
+    Args:
+        model_dir: the root folder containing the checkpoint state file
+        output_node_names: a string, containing all the output node's names, 
+                            comma separated
+    """
+    if not tf.gfile.Exists(model_dir):
+        raise AssertionError(
+            "Export directory doesn't exists. Please specify an export "
+            "directory: %s" % model_dir)
+
+    if not output_node_names:
+        print("You need to supply the name of a node to --output_node_names.")
+        return -1
+
+    # We retrieve our checkpoint fullpath
+    checkpoint = tf.train.get_checkpoint_state(model_dir)
+    input_checkpoint = checkpoint.model_checkpoint_path
+    
+    # We precise the file fullname of our freezed graph
+    absolute_model_dir = "/".join(input_checkpoint.split('/')[:-1])
+    output_graph = absolute_model_dir + "/frozen_model.pb"
+
+    # We clear devices to allow TensorFlow to control on which device it will load operations
+    clear_devices = True
+
+    # We start a session using a temporary fresh Graph
+    with tf.Session(graph=tf.Graph()) as sess:
+        # We import the meta graph in the current default Graph
+        saver = tf.train.import_meta_graph(input_checkpoint + '.meta', clear_devices=clear_devices)
+
+        # We restore the weights
+        saver.restore(sess, input_checkpoint)
+
+        # We use a built-in TF helper to export variables to constants
+        output_graph_def = tf.graph_util.convert_variables_to_constants(
+            sess, # The session is used to retrieve the weights
+            tf.get_default_graph().as_graph_def(), # The graph_def is used to retrieve the nodes 
+            output_node_names.split(",") # The output node names are used to select the usefull nodes
+        ) 
+        # fix nodes
+        for node in output_graph_def.node:
+            if node.op == 'RefSwitch':
+                node.op = 'Switch'
+                for index in range(len(node.input)):
+                    if 'moving_' in node.input[index]:
+                        node.input[index] = node.input[index] + '/read'
+            elif node.op == 'AssignSub':
+                node.op = 'Sub'
+                if 'use_locking' in node.attr: del node.attr['use_locking']
+            elif node.op == 'AssignAdd':
+                node.op = 'Add'
+                if 'use_locking' in node.attr: del node.attr['use_locking']
+            elif node.op == 'Assign':
+                node.op = 'Identity'
+                if 'use_locking' in node.attr: del node.attr['use_locking']
+                if 'validate_shape' in node.attr: del node.attr['validate_shape']
+                if len(node.input) == 2:
+                    # input0: ref: Should be from a Variable node. May be uninitialized.
+                    # input1: value: The value to be assigned to the variable.
+                    node.input[0] = node.input[1]
+                    del node.input[1]
+
+        tf.import_graph_def(output_graph_def, name='')
+        tf.train.write_graph(output_graph_def, './', 'good_frozen.pb', as_text=False)
+        print("%d ops in the final graph." % len(output_graph_def.node))
+
+    return 
+
